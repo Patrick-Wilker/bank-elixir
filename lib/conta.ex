5146dc3a68ee5c7ebf4bb9b3542bb3c7d27f54f6
Patrick <patrickvieiramota@gmail.com>
2020-12-25 17:47:15 -0300

    feat: Create txt file to work with account data

--- lib/conta.ex
+++ lib/conta.ex
@@ -1,18 +1,37 @@
 defmodule Conta do
   defstruct usuario: Usuario, saldo: 1000
+  @contas "contas.txt"
+  def cadastrar(usuario) do
+    case buscar_por_email(usuario.email) do
+      nil ->
+        binary = [%__MODULE__{usuario: usuario}]  ++ busca_contas()
+        |> :erlang.term_to_binary()
+        File.write(@contas, binary)
+      _ -> {:error, "Conta ja cadastrada!"}
+    end
 
-  def cadastrar(usuario), do: %__MODULE__{usuario: usuario}
+  end
 
-  def transferir(contas, de, para, valor) do
-    de = Enum.find(contas, fn conta -> conta.usuario.email == de.usuario.email end)
+  def busca_contas do
+    {:ok, binary} = File.read(@contas)
+    :erlang.binary_to_term(binary)
+  end
+
+  def buscar_por_email(email), do: Enum.find(busca_contas(), &(&1.usuario.email == email))
+
+  def transferir(de, para, valor) do
+    de = buscar_por_email(de.usuario.email)
 
     cond do
       valida_saldo(de.saldo, valor) -> {:error, "Saldo insuficiente!"}
       true ->
-        para = Enum.find(contas, fn conta -> conta.usuario.email == para.usuario.email end)
+        contas = busca_contas()
+        contas = List.delete contas, de
+        contas = List.delete contas, para
         de = %Conta{de | saldo: de.saldo - valor}
         para = %Conta{para | saldo: para.saldo + valor}
-      {de, para}
+        contas = contas ++ [de, para]
+        File.write(@contas, :erlang.term_to_binary(contas))
     end
   end
 
@@ -20,10 +39,15 @@ defmodule Conta do
     cond do
       valida_saldo(conta.saldo, valor) -> {:error, "Saldo insuficiente!"}
       true ->
+        contas = busca_contas()
+        contas = List.delete contas, conta
         conta = %Conta{conta | saldo: conta.saldo - valor}
+        contas = contas ++ [conta]
+        File.write(@contas, :erlang.term_to_binary(contas))
         {:ok, conta, "Mensagem de e-mail encaminhada"}
     end
   end
 
+  # defp é uma funcao privada
   defp valida_saldo(saldo, valor), do: saldo < valor
 end
